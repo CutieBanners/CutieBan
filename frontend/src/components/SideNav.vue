4c7fef7a5558cbe84a0ed46253ffff983e2ffa94
JuilletMikael <mikael.juillet@eduvaud.ch>
2024-11-25 15:11:17 +0100

    feat: create side nav vue

---
+++ frontend/src/components/SideNav.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {Button, Drawer, Avatar} from "primevue";
+
+
+import {ref} from "vue";
+const visible = ref(false)
+
+const saved = ["Saved 1", "Saved 2"]
+
+</script>
+
+<template>
+  <div class="card flex justify-center">
+    <Drawer v-model:visible="visible" header="CutieBan">
+      <h3><i class="pi pi-bookmark-fill"></i> Saved</h3>
+      <div></div>
+    </Drawer>
+    <Button icon="pi pi-arrow-right" @click="visible = true" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
