34be4eec94ec59d60fb6f1b6a7fbf1f88f82e525
eliott.jaquier <eliott.jaquier@eduvaud.ch>
2024-12-02 16:19:42 +0100

    feat: Add injectable crud service

--- frontend/src/models/ProjectModel.ts
+++ frontend/src/models/ProjectModel.ts
@@ -1,10 +1,12 @@
 import {PostItListModel} from "./PostItListModel.ts";
 
 export class ProjectModel {
+    public id: number;
     public title: string;
     public postItList: PostItListModel[];
 
-    constructor(title: string, postItList: PostItListModel[]) {
+    constructor(id: number, title: string, postItList: PostItListModel[]) {
+        this.id = id;
         this.title = title;
         this.postItList = postItList;
     }
