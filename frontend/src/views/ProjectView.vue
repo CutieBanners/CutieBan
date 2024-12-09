60ccea0fdbebf091982cfe32f6caef97e3557a6a
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-12-09 14:48:07 +0100

    feat: watch for route param change

--- frontend/src/views/ProjectView.vue
+++ frontend/src/views/ProjectView.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import Project from "@/components/Project.vue";
-import {inject} from "vue";
-import {CrudService} from "@/services/CrudService.ts";
-import {useRoute} from "vue-router";
+import { inject, ref, watch } from "vue";
+import { CrudService } from "@/services/CrudService.ts";
+import { useRoute } from "vue-router";
 
-const crudService : CrudService = inject('crudService')!;
+const crudService: CrudService = inject('crudService')!;
 const route = useRoute();
-const projectId : number = Number(route.params.id);
-const project = crudService.getProject(projectId);
-console.log(project);
+
+// Reactive reference for project
+const project = ref(null);
+
+// Function to load the project
+const loadProject = (id: number) => {
+  project.value = crudService.getProject(id);
+};
+
+watch(
+    () => route.params.id,
+    (newId, oldId) => {
+      loadProject(Number(newId));
+    },
+    { immediate: true }
+);
 </script>
 
 <template>
@@ -16,5 +29,4 @@ console.log(project);
 </template>
 
 <style scoped>
-
 </style>
