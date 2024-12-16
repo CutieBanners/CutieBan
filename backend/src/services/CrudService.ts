8b095b3c62deea0e0b1796a32341c8459e7c8210
MinusW <pw07vck@eduvaud.ch>
2024-12-16 15:48:22 +0100

    docs: document CrudService

--- backend/src/services/CrudService.ts
+++ backend/src/services/CrudService.ts
@@ -3,10 +3,18 @@ import { PostItListModel} from '../models/PostItListModel';
 import { PostItModel} from '../models/PostItModel';
 import { ProjectModel} from '../models/ProjectModel';
 
-
+/**
+ * Service class for CRUD operations on projects.
+ * This class is responsible for creating, reading, updating, and deleting projects.
+ */
 export class CRUDService {
     constructor(private dbService: DatabaseService) {}
 
+    /**
+     * Creates a new project and gives it MongoDB ID as id.
+     * @param title - The title of the project.
+     * @returns The created project.
+     */
     async createProject(title: string): Promise<ProjectModel> {
         const project = {
             title: title,
@@ -17,6 +25,11 @@ export class CRUDService {
         return project;
     }
 
+    /**
+     * Retrieves a project by its MongoDB ID.
+     * @param projectId - The ID of the project to retrieve.
+     * @returns The project with the specified ID.
+     */
     async getProject(projectId: string): Promise<ProjectModel> {
         let project = await this.dbService.findOne<ProjectModel>(projectId);
         if (!project) {
@@ -30,6 +43,12 @@ export class CRUDService {
         return project;
     }
 
+    /**
+     * updates a project by its MongoDB ID.
+     * @param projectId - The ID of the project to update.
+     * @param project - The updated project data.
+     * @returns The number of documents updated.
+     */
     async updateProject(projectId: string, project: ProjectModel ): Promise<number> {
         const existingProject = await this.dbService.findOne<ProjectModel>(projectId);
         if (!existingProject) {
@@ -41,6 +60,12 @@ export class CRUDService {
         return await this.dbService.updateOne(projectId, project);
     }
 
+
+    /**
+     * Deletes a project by its MongoDB ID.
+     * @param projectId
+     * @returns number
+     */
     async deleteProject(projectId: string): Promise<number> {
         const project = await this.dbService.findOne<ProjectModel>(projectId);
         if (!project) {
@@ -50,6 +75,12 @@ export class CRUDService {
         return await this.dbService.deleteOne(projectId);
     }
 
+    /**
+     * Sorts a list of <T> by order.
+     * @constraint <T> must have an order property.
+     * @param list
+     * @returns list sorted by order
+     */
     private sortByOrder<T extends { order: number }>(list: T[]): T[] {
         return list.sort((a, b) => a.order - b.order);
     }
